7dfe6ffe7254c7c85b3403dfc6decc7656423a27
Arthur Vinícius <arthurvcs2@gmail.com>
2018-10-28 16:51:15 -0300

    Alterações no grafo e menu

--- TPGrafos/Classes/Grafo.cs
+++ TPGrafos/Classes/Grafo.cs
@@ -12,6 +12,7 @@ namespace TPGrafos.Classes
     {
         protected ListaVertice vertices;
         protected ListaAresta arestas;
+        public bool digrafo { get { return this.digrafo; } set { this.digrafo = value; } }
 
         public Grafo()
         {}
@@ -34,15 +35,15 @@ namespace TPGrafos.Classes
         /// True: É orientado 
         /// False: não é orientado
         /// </returns>
-        private static bool IsDigrafo(string arquivo)
+        private bool IsDigrafo(string arquivo)
         {
             string[] linhas = arquivo.Replace("\r", "").Split('\n');
             if(linhas.Length == 1)
-            { return false; }
+            { return this.digrafo = false; }
             linhas = linhas[1].Replace("\r", "").Split('\n', ';');
 
-            if (linhas.Length == 3) { return false; }
-            else { return true; }
+            if (linhas.Length == 3) { return this.digrafo  =false; }
+            else { return this.digrafo = true; }
         }
 
         /// <summary>
@@ -50,7 +51,7 @@ namespace TPGrafos.Classes
         /// </summary>
         /// <param name="caminho">o arquivo que o usuário informa</param>
         /// <returns>Retorna um grafo</returns>
-        protected static Grafo GeraGrafo(Stream caminho)
+        protected  Grafo GeraGrafo(Stream caminho)
         {
             Grafo g;
             ListaVertice vertices = new ListaVertice();
---
+++ TPGrafos/Menu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TPGrafos.Classes;
+using System.IO;
+using TPGrafos.Classes.Estruturas;
+
+namespace TPGrafos
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void AbirArquivo_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog biblioteca = new OpenFileDialog();
+            biblioteca.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            if (biblioteca.ShowDialog() == DialogResult.OK)
+            {
+                GNaoDirigido grafo = new GNaoDirigido();
+                    grafo = (GNaoDirigido)grafo.GetGrafo(biblioteca.OpenFile());
+                grafo.IsAdjacente(new Vertice(1) ,new Vertice(3));
+            }
+            MessageBox.Show("show");
+        }
+    }
+}
+    
+
